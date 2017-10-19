78c976f3b762e4ccfc5a97f512360892010722f4
Anchit <anc.shukla@gmail.com>
2017-10-19 19:05:31 +0530

    Add signup form validation on the backend.

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,12 @@
 from django.forms import ModelForm
 from django import forms
-from .models import User
-
 
 class UserSignupForm(forms.Form):
 
     first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
     last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
     username = forms.CharField(max_length=16, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
-    email = forms.EmailField()
+    email = forms.CharField(max_length=75)
     password = forms.CharField(max_length=64, widget=forms.PasswordInput)
     password_confirm = forms.CharField(max_length=64, widget=forms.PasswordInput)
 
--- blog/views.py
+++ blog/views.py
@@ -16,10 +16,19 @@ def index(request):
 
 def signup(request):
     new_user = User()
+    err_list = []
     if request.method == 'POST':
         form = UserSignupForm(request.POST)
-        print ('username entered : ' + request.POST['username'])
-        if form.is_valid():
+        if User.objects.filter(username=request.POST['username']).exists():
+            err_list.append('username already exists.')
+        if User.objects.filter(email=request.POST['email']).exists():
+            err_list.append('An account with the entered email already exists.')
+        if request.POST['password'] != request.POST['password_confirm']:
+            err_list.append('The entered passwords do not match.')
+        if len(request.POST['password']) < 6:
+            err_list.append('The password is too short. (min. 6 characters')
+
+        if form.is_valid() and len(err_list) == 0:
             new_user.first_name = form.cleaned_data['first_name']
             new_user.last_name = form.cleaned_data['last_name']
             new_user.username = form.cleaned_data['username']
@@ -27,10 +36,9 @@ def signup(request):
             p_hash = make_password(form.cleaned_data['password'])
             new_user.password_hash = p_hash
             new_user.save()
-
     else:
         form = UserSignupForm()
-    context = {'title_main': title_main, 'title_sub': 'Sign Up', 'form': form, }
+    context = {'title_main': title_main, 'title_sub': 'Sign Up', 'form': form, 'err_list': err_list}
     return render(request, 'blog/signup.html', context)
 
 
