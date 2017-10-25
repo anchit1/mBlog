7d1f35a1aa6451bcc8d4554ce56b50e05d903010
Anchit <anc.shukla@gmail.com>
2017-10-25 16:21:53 +0530

    Add logout button to feed header

--- blog/static/js/feed.js
+++ blog/static/js/feed.js
@@ -1,9 +1,22 @@
-$('#new-post-input').focus(function () {
-    $('#new-post-bottom').removeClass('inactive');
-    $('#new-post-bottom').addClass('active');
+// need to make the bottom bar of the new post form
+// visible upon focus
+
+// language=JQuery-CSS
+var new_post_input = $('#new-post-input');
+var new_post_bottom = $('#new-post-bottom');
+new_post_input.focus(function () {
+    new_post_bottom.removeClass('inactive');
+    new_post_bottom.addClass('active');
 });
 
-$('#new-post-input').blur(function () {
-    $('#new-post-bottom').removeClass('active');
-    $('#new-post-bottom').addClass('inactive');
+new_post_input.blur(function () {
+    new_post_bottom.removeClass('active');
+    new_post_bottom.addClass('inactive');
+});
+
+var header_profile_pic = $('#header-profile-pic');
+header_profile_pic.click(function () {
+    var dropdown = $('.dropdown');
+    dropdown.removeClass('inactive')
+    dropdown.addClass('active');
 });
